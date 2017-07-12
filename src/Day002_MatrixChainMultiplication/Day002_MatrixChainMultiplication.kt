358bca5bf697e7b5fc4c244a2b8333a1c044c054
kfwong <kfwong@zoho.com>
2017-07-12 12:34:07 +0800

    Added some description and memoization.

--- src/Day002_MatrixChainMultiplication/Day002_MatrixChainMultiplication.kt
+++ src/Day002_MatrixChainMultiplication/Day002_MatrixChainMultiplication.kt
@@ -4,19 +4,25 @@ fun main(args: Array<String>) {
     println(mcm(intArrayOf(1, 2, 3, 4, 3), 1, 4))
 }
 
-// recursive solution
+// recursive solution with memoization
+val memo = Array(5, { IntArray(5) })
+
 fun mcm(p: IntArray, i: Int, j: Int): Int {
+    // base case
     if (i == j) return 0;
+
+    // return memo if computed before
+    if (memo[i][j] > 0) return memo[i][j]
 
     var min = Int.MAX_VALUE;
 
-    for ( k in i..j-1) {
-        var count = Math.min(
+    for ( k in i..j - 1) {
+        min = Math.min(
                 min,
                 mcm(p, i, k) + mcm(p, k + 1, j) + p[i - 1] * p[k] * p[j]
         );
 
-        if(count < min) min = count;
+        memo[i][j] = min;
     }
 
     return min;
